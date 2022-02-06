79cfe2e540348f964d5c2439d67d54d2830dc14c
mohankrishna1990 <95756625+mohankrishna1990@users.noreply.github.com>
2022-02-06 14:49:05 +0530

    Update AppTests.java

--- src/test/java/com/bit/wonders/application/AppTests.java
+++ src/test/java/com/bit/wonders/application/AppTests.java
@@ -1,7 +1,11 @@
 package com.bit.wonders.application;
 
+
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,40 +17,49 @@ import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 
+
+
 import com.bit.wonders.web.HelloController;
+
+
 
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("dev")
 public class AppTests {
 
-	@LocalServerPort
-	private int port;
-	
-	@Autowired
-	private HelloController homeController;
 
-	@Autowired
-	private TestRestTemplate testRestTemplate;
-	
-	@Test
-	@DisplayName(value = "Loads application context and verifies if OK.")
-	void contextLoads() {
-		assertNotNull(homeController);
-	}
 
-	@Test
-	@DisplayName(value = "Test controller")
-	void testController() {
-		assertEquals(homeController.projectVersion(), "1.0", "Seems fine");
-		assertEquals(homeController.helloWorld(), "This is dev");
-	}
-	
-	@Test
-	@DisplayName(value = "Test controller using TestRestTemplate")
-	void testTemplate() {
-		assertNotNull(testRestTemplate);
-		ResponseEntity<String> response = testRestTemplate.getForEntity("/hello", String.class);
-		String repsonseText = response.getBody();
-		assertEquals(repsonseText,"This is dev");
-	}
+@LocalServerPort
+private int port;
+
+@Autowired
+private HelloController homeController;
+
+
+
+@Autowired
+private TestRestTemplate testRestTemplate;
+
+@Test
+@DisplayName(value = "Loads application context and verifies if OK.")
+void contextLoads() {
+assertNotNull(homeController);
+}
+
+
+
+@Test
+@DisplayName(value = "Test controller")
+void testController() {
+assertEquals(homeController.projectVersion(), "Version - 1.0", "Seems fine");
+assertEquals(homeController.helloWorld(), "Hello World @ Azure.");
+}
+
+@Test
+@DisplayName(value = "Test controller using TestRestTemplate")
+void testTemplate() {
+assertNotNull(testRestTemplate);
+ResponseEntity<String> response = testRestTemplate.getForEntity("/hello", String.class);
+String repsonseText = response.getBody();
+assertEquals(repsonseText,"Hello World @ Azure.");
 }
